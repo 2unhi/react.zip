a0dd4cc6fb3d13dc0d930fb17bc88366400fcc07
2unhi <ironhak1106@gmail.com>
2024-05-17 14:05:35 +0900

    dice output

--- dicegame/src/index.js
+++ dicegame/src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>주사위 게임</h1>);
+ReactDOM.render(<App />, document.getElementById('root'));
